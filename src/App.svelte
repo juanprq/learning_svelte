6c1720d1674d2ab66b0cd2ce634917e9e9bfa825
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-04-18 14:29:57 -0500

    adds tabs menu

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,15 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
-</script>
+  import Tabs from './shared/Tabs.svelte';
 
-<Header />
-<main>
-  <p>
-  orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  </p>
-</main>
-<Footer />
+  let items = ['Current Polls', 'Add New Poll'];
+  let activeItem = items[0];
+
+  const handleOnTabChange = (event) => {
+    activeItem = event.detail;
+  };
+</script>
 
 <style>
   main {
@@ -17,3 +17,11 @@
     margin: 40px auto;
   }
 </style>
+
+<Header />
+<main>
+  <Tabs {items} {activeItem} on:tabChange={handleOnTabChange}>
+  </Tabs>
+</main>
+<Footer />
+
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let items;
+  export let activeItem;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<style>
+  .tabs {
+    margin-bottom: 40px;
+  }
+  ul {
+    display: flex;
+    padding: 0;
+    justify-content: center;
+    list-style-type: none;
+    font-size: 18px;
+    cursor: pointer;
+  }
+  li {
+    margin: 0 16px;
+    color: #555;
+  }
+  .active {
+    color: #d91b42;
+    border-bottom: 2px solid #d91b42;
+    padding-bottom: 8px;
+  }
+</style>
+
+<div class="tabs">
+  <ul>
+    {#each items as item}
+      <li on:click={() => dispatch('tabChange', item)}>
+        <div class:active={activeItem === item}>
+          {item}
+        </div>
+      </li>
+    {/each}
+  </ul>
+</div>
