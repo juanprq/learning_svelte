4187440ac02f3b630e5ad339e5dbcb3ffb12b598
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-04-27 17:31:21 -0500

    Adds voting bars

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -6,6 +6,9 @@
   const dispatch = createEventDispatcher();
 
   $: totalVotes = poll.voteA + poll.voteB;
+  $: percentA = Math.ceil(poll.voteA / totalVotes * 100);
+  $: percentB = Math.ceil(poll.voteB / totalVotes * 100);
+
   const handleOnVote = (option, questionId) => dispatch('vote', { option, questionId });
 </script>
 <style>
@@ -32,6 +35,19 @@
     display: inline-block;
     padding: 10px 20px;
   }
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a {
+    background-color: rgba(217, 20, 66, 0.2);
+    border-left: 4px solid #d91b42;
+  }
+  .percent-b {
+    background-color: rgba(69, 196, 150, 0.2);
+    border-left: 4px solid #45c496;
+  }
 </style>
 
 <Card>
@@ -43,11 +59,11 @@
       Total votes: {totalVotes}
     </p>
     <div class="answer" on:click={() => handleOnVote('a', poll.id)}>
-      <div class="percent percent-a"></div>
+      <div class="percent percent-a" style="width: {percentA}%"></div>
       <span>{poll.answerA} ({poll.voteA})</span>
     </div>
     <div class="answer" on:click={() =>handleOnVote('b', poll.id)}>
-      <div class="percent percent-b"></div>
+      <div class="percent percent-b"style="width: {percentB}%"></div>
       <span>{poll.answerB} ({poll.voteB})</span>
     </div>
   </div>
